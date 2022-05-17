3e4aa4be15bdc1e24bb6d63f6869ea6c8c3c7ebd
MarcoPadeiroIPL <2211868@my.ipleiria.pt>
2022-05-17 21:26:38 +0100

    Adição do PHPActiveRecord através do composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bfbac5c6a090cff660af962bab66acba96a311c9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bfbac5c6a090cff660af962bab66acba96a311c9',
+            'dev_requirement' => false,
+        ),
+        'php-activerecord/php-activerecord' => array(
+            'pretty_version' => 'v1.2.0',
+            'version' => '1.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-activerecord/php-activerecord',
+            'aliases' => array(),
+            'reference' => 'e6d2dad090617336dee730190be0294c5f132c69',
+            'dev_requirement' => false,
+        ),
+    ),
+);
